15c74ce8cfeb7706018bb2f08f6a9adcb6d4a588
TW <timberwatch.work@gmail.com>
2022-01-25 11:48:03 +0500

    Add city cookie

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from .models import Product
 
+
+def cookie_city(request):
+    city = '!'
+    if request.method == 'GET':
+        if 'city' in request.COOKIES:
+            city = request.COOKIES['city']
+    elif request.method == 'POST':
+        city = request.POST.get('city')
+
+    return city
+
+
 def index(request):
-    Products = Product.objects.all()
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'Products': Products})
+    city = cookie_city(request)
+    products = Product.objects.all()
+    response = render(request, 'main/index.html', {'title': 'Главная страница сайта', 'products': products,
+                                                   'city': city,})
+    response.set_cookie('city', city)
+    return response
 
 
 def cart(request):
-    return render(request, 'main/cart.html')
+    city = cookie_city(request)
+    response = render(request, 'main/cart.html', {'city': city, })
+    response.set_cookie('city', city)
+    return response
+
